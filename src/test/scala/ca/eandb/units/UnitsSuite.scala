a7d856df81e007c399aa9dae4583a49701d70c92
Brad Kimmel <intrepidca@gmail.com>
2012-12-29 13:22:32 -0500

    Add test suite for Units operations

---
+++ src/test/scala/ca/eandb/units/UnitsSuite.scala
@@ -0,0 +1,66 @@
+/**
+ * Scala Units
+ * Copyright (C) 2012 Bradley W. Kimmel
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package ca.eandb.units
+
+import org.scalatest.FunSuite
+import org.scalatest.BeforeAndAfter
+
+import Helpers._
+
+class UnitsSuite extends FunSuite with BeforeAndAfter {
+
+  var u: UnitsParser = _
+  var m: Units = _
+  var in: Units = _
+  var ft: Units = _
+
+  before {
+    u = new UnitsParser
+    u.define("m !")
+    u.define("c- 1e-2")
+    u.define("inch 2.54 cm")
+    u.define("foot 12 inch")
+    u.define("in inch")
+    u.define("ft foot")
+    
+    m = u("m")
+    in = u("in")
+    ft = u("ft")
+  }
+
+  // Even though 12 inches is exactly one foot, if we naively convert to
+  // canonical units (meters), divide, then express the result in feet, we get
+  // slightly less than 1 ft due to divisions by decimal quantities.  In this
+  // case, "12 (in) inOneOf (ft, in)" will result in "12 (in)".  This test
+  // ensures that we correctly handle this case and return a result of 1 (ft).
+  test("no loss of precision when rational conversion possible") {
+    val actual = 12 (in) inOneOf (ft, in)
+    val expected = 1 (ft)
+    assert(actual === expected)
+  }
+
+}
+
